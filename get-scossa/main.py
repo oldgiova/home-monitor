9bc7eb91c4082b50056bbed3e78d3d777baf53bc
Roberto Giovanardi <rob.giovanardi@gmail.com>
2018-12-26 10:01:51 +0100

    added logging

--- get-scossa/main.py
+++ get-scossa/main.py
@@ -1,6 +1,6 @@
 from gpiozero import LightSensor
 from influxdb import InfluxDBClient
-import time,datetime
+import time,datetime,logging
 
 '''ldr IO number and sensitivity'''
 ldr = LightSensor(4,1)
@@ -14,11 +14,18 @@ influxdb_host = 'localhost'
 influxdb_port = 8086
 influxdbclient = InfluxDBClient(influxdb_host, influxdb_port, influxdb_user, influxdb_password, influxdb_db)
 
+'''logging config'''
+logging.basicConfig(
+        level=logging.WARNING,
+        filename='ldr.log',
+        format='[%(asctime)s] %(levelname)s:%(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
 
 '''functions'''
 def ldr_get():
     ldr.wait_for_light()
     '''sleep a bit for not getting again the same pulse'''
+    logging.debug('pulse occurred ')
     time.sleep(0.5)
     return True
 
@@ -37,10 +44,12 @@ def insert_influxdb_row():
                     }
                 }
             ]
+    logging.debug('writing a value to influxdb with time ', now)
     influxdbclient.write_points(json_body)
 
 
 def main():
+    logging.info('Starting to ldr ')
     while True:
         ldr_get()
         insert_influxdb_row()
